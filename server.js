28241430a646c0c2f1c4f679ba3045fdc7aa758f
Baibhav KC <baibhavkc11@gmail.com>
2023-08-29 13:19:14 +0545

    basic setup for the server

---
+++ server.js
@@ -0,0 +1,14 @@
+// Importing dependencies
+const express = require('express');
+
+// Setup the express server
+const app = express();
+const port = 3000;
+
+// Importing middlewares into express
+app.use(express.json());
+
+// At last starting the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
